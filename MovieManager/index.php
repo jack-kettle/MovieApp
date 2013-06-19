dbac8d033fc51f0f955115a3865ea43106230fc5
Jack Kettle <jack.kettle2@gmail.com>
2013-06-19 22:51:30 +0100

    GEN: Converted code to function getFileTypes

--- MovieManager/index.php
+++ MovieManager/index.php
@@ -1,19 +1,23 @@
 <?php
 
-$sPath = "C:/Users/jack kettle/Zip Test/";
+$sPath = "C:/Users/Jacks Desktop/Documents/";
 
-if ($o_handle = opendir($sPath)) {
-
-	$o_file_info = new finfo();
-
-    while ($entry = readdir($o_handle)) {
-		@$s_mime_type = $o_file_info->buffer(
-				file_get_contents($sPath.$entry), FILEINFO_MIME_TYPE);
-		if( $s_mime_type != "application/x-empty" )
-		{
-			echo $s_mime_type."<br>"; 
-		}
+function getFileTypes($s_Path){
+    @$o_handle = opendir($s_Path);
+    if ($o_handle != NULL) {
+        $o_file_info = new finfo();
+        while ($s_entry = readdir($o_handle)) {
+            @$s_mime_type = $o_file_info->buffer(
+                    file_get_contents($s_Path.$s_entry), FILEINFO_MIME_TYPE);
+            if( $s_mime_type != "application/x-empty" )
+            {
+                echo $s_mime_type."<br>"; 
+            }
+        }
+        closedir($o_handle);
+    }else{
+        echo 'Folder does not exsist';
     }
-
-    closedir($o_handle);
 }
+
+getFileTypes($sPath);
